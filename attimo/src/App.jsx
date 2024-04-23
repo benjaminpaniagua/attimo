a26273fdff91bc618209077519be8022c251ddd4
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2024-04-22 21:19:17 -0600

    added project architecture

---
+++ attimo/src/App.jsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export function LogIn() {
+    return (
+        <>
+        <h1 className="text-9xl">Hola</h1>
+        </>
+    );
+}
+
--- attimo/src/main.jsx
+++ attimo/src/main.jsx
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <h1 className='text-9xl'>Hola</h1>
+  <h1 className='text-black'>Hola</h1>
 )
