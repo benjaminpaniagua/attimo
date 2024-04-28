7b3a4dd20b9003418b0ac7d3736f9f481f34baef
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2024-04-28 17:29:21 -0600

    adjust donut

--- attimo/src/components/UI/DonutChar.jsx
+++ attimo/src/components/UI/DonutChar.jsx
@@ -5,7 +5,7 @@ export default function DonutChar({ data }) {
   return (
     <div className='mt-12'>
     <PieChart
-    margin={{ top: 0, bottom: 0, left: 0, right: 0 }}
+    margin={{ top: 0, bottom: 80, left: 0, right: 0 }}
       series={[
         {
           data,
@@ -13,7 +13,7 @@ export default function DonutChar({ data }) {
           faded: { innerRadius: 20, additionalRadius: -30, color: 'grey' },
         },
       ]}
-      height={300} // Adjust height of the chart
+      height={300}
         legend={{
           direction: 'row',
           position: { vertical: 'bottom', horizontal: 'middle' },
