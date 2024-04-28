78a10fcf7bf4a6204d0d8bf39e8c6416180525ed
benjaminpaniagua <benjaminjosepaniaguarojas@gmail.com>
2024-04-27 22:58:22 -0600

    Donut semi ready

--- attimo/src/components/UI/DonutChar.jsx
+++ attimo/src/components/UI/DonutChar.jsx
@@ -5,6 +5,7 @@ export default function DonutChar({ data }) {
   return (
     <div className='mt-12'>
     <PieChart
+    margin={{ top: 0, bottom: 0, left: 0, right: 0 }}
       series={[
         {
           data,
@@ -12,8 +13,13 @@ export default function DonutChar({ data }) {
           faded: { innerRadius: 20, additionalRadius: -30, color: 'grey' },
         },
       ]}
-      height={300}
-    />
+      height={300} // Adjust height of the chart
+        legend={{
+          direction: 'row',
+          position: { vertical: 'bottom', horizontal: 'middle' },
+          padding: 1,
+        }}
+      />
     </div>
   );
 }
