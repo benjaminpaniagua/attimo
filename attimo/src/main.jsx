07c63f09d8ae102d6b2f54b62152adcc132ab5e7
PuzzledStone <ianxd03@gmail.com>
2024-04-29 20:24:35 -0600

    latest stable version

--- attimo/src/main.jsx
+++ attimo/src/main.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {HomePage} from './HomePage.jsx'
 import {Events} from './Events.jsx'
+import { Home } from 'lucide-react'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <>
-      <Events />
+      <Events/>
     </>
 )
