5013315306a6e175cc38b9d906691992d20dc06d
Ashley Pérez <ashleycr33@gmail.com>
2024-05-01 19:42:13 -0600

    Styles in InputSearh fixed

--- attimo/src/components/UI/InputSearch.jsx
+++ attimo/src/components/UI/InputSearch.jsx
@@ -4,14 +4,14 @@ import "../../index.css";
 
 export function InputSearch({ placeholder }) {
     return (
-      <div className="flex justify-between bg-clr-light-gray rounded-md overflow-hidden text-clr-white">
+      <div className="flex justify-between px-4 bg-clr-light-gray/40 dark:bg-clr-dark-gray/30 rounded-md overflow-hidden text-clr-dark-bg">
         <input
-          className="w-full py-2 placeholder:text-white px-4 bg-transparent cursor-pointer focus:outline-none"
+          className="w-full py-2 placeholder:text-clr-dark-gray bg-transparent dark:placeholder:text-clr-light-gray cursor-pointer focus:outline-none"
           type="text"
           placeholder={placeholder} // Usar el prop 'placeholder' aquí
           name="search"
         />
-        <Search size={24} className="px-2 bg-clr-dark-gray h-full" />
+        <Search size={18} className="text-clr-dark-gray m-auto h-full dark:text-clr-light-gray" />
       </div>
     );
   }
