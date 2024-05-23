7894017d0eb9238185a8f0b166ae17c93c1245b1
Ashley Pérez <ashleycr33@gmail.com>
2024-05-23 08:48:36 -0600

    Fixing edit image in profile

--- src/components/UI/CardEditInfo.jsx
+++ src/components/UI/CardEditInfo.jsx
@@ -1,7 +1,7 @@
 import "../../index.css";
-
+import { Pencil } from "lucide-react"; 
+import React from "react";
 export function CardEditInfo({title,img}) {
-
     return(
         <>
             <div className='grid justify-center text-center'>
@@ -10,18 +10,8 @@ export function CardEditInfo({title,img}) {
                     <img src={img} className="w-full rounded-full" alt="profile photo" />
                     <button className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                         <div className="bg-gray-500 opacity-50 rounded-full w-full h-full flex items-center justify-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" 
-                            width="24" 
-                            height="24" 
-                            viewBox="0 0 24 24" 
-                            fill="none" 
-                            stroke="currentColor" 
-                            stroke-width="2" 
-                            stroke-linecap="round" 
-                            stroke-linejoin="round" 
-                            className="lucide lucide-pencil w-6 h-6 text-white opacity-100">
-                            <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z"/><path d="m15 5 4 4"/>
-                            </svg>
+                            <input type="file" accept=".jpg, .jpeg, .png, .webp" className="w-full cursor-pointer h-full opacity-0 absolute"/>
+                            <Pencil className="text-white"/>
                         </div>
                     </button>
                 </div>
