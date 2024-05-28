04cb4764d79d4a672c38adefc981fa56a4244074
Ashley Pérez <ashleycr33@gmail.com>
2024-05-27 22:08:39 -0600

    Final revision

--- src/components/activity/UpcomingEvents.jsx
+++ src/components/activity/UpcomingEvents.jsx
@@ -9,7 +9,7 @@ export function UpcomingEvents({ items }) {
     <div className="mt-2 px-6 grid">
       <section className="flex items-center justify-between">
         <h2 className="my-4 dark:text-white">Upcoming events</h2>
-        <Link to="/events"><p className="duration-300 cursor-pointer hover:text-clr-blue dark:text-clr-light-gray dark:hover:text-white">View all</p></Link>
+        <Link to="/attimo/events"><p className="duration-300 cursor-pointer hover:text-clr-blue dark:text-clr-light-gray dark:hover:text-white">View all</p></Link>
       </section>
       
       {items.length === 0 ? (
