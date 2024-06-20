fbe78ab4aa4c2fd8a29d1c7cfa384b78a097f3fb
Ashley Pérez <ashleycr33@gmail.com>
2024-06-19 22:27:56 -0600

    fix: solving counting problem in activities reports mini cards

--- src/components/activity/SectionTasks.jsx
+++ src/components/activity/SectionTasks.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Task } from "../UI/Task.jsx";
-import useActivities from "../hooks/useActivities";
+import { useFetchActivities } from "../hooks/useFetchActivities";
 
 export function Tasks() {
   const user = JSON.parse(localStorage.getItem("user"));
   const userId = user ? user.id : null;
 
-  const { activities, remainingTasks } = useActivities(userId);
-
-  const inactiveActivities = activities.filter(activity => activity.status !== 'Active');
+  const { data } = useFetchActivities(userId);
+  const activeActivities = data.filter(activity => activity.status === 'Active');
+  const inactiveActivities = data.filter(activity => activity.status === 'Inactive');
 
   return (
     <div className="flex w-full gap-4 md:flex-col">
@@ -16,7 +16,7 @@ export function Tasks() {
         <Task number={inactiveActivities.length} description="Inactive Activities" />
       </section>
       <section className="w-1/2 grid items-center content-center bg-white dark:bg-clr-dark-third p-4 text-center rounded-lg md:py-8 md:w-full">
-        <Task number={remainingTasks} description="Remaining Activities" />
+        <Task number={activeActivities.length} description="Remaining Activities" />
       </section>
     </div>
   );
